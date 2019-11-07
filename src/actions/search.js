2f05bbb790574cbe9f7c171ba5c77171d16c6099
kwang429 <katherine.wang429@gmail.com>
2019-11-06 18:14:58 -0800

    Pass action and reducer tests

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,17 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+
   //TODO:  Write an asynchronous action to handle a video search!
+  var options = {
+    key: YOUTUBE_API_KEY,
+    query: q
+  };
+
+  return {
+    videos: searchYouTube(options, changeVideoList)
+  };
+
 };
 
 export default handleVideoSearch;
